7777b7632f0e34ea6c7abb2148453533e48859c5
SziNo <szino1848@gmail.com>
2025-03-25 00:15:05 +0100

    update client.ts

--- firebase/client.ts
+++ firebase/client.ts
@@ -9,13 +9,13 @@ import { getFirestore } from 'firebase/firestore'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyBmADY4NAaYOIvqp3_Akd1YcDtHUAfXyFA',
-  authDomain: 'prepwise-2d137.firebaseapp.com',
-  projectId: 'prepwise-2d137',
-  storageBucket: 'prepwise-2d137.firebasestorage.app',
-  messagingSenderId: '302975089836',
-  appId: '1:302975089836:web:e6d274a650c7e00c4a3309',
-  measurementId: 'G-8T8MREXZ25',
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 }
 
 // Initialize Firebase
