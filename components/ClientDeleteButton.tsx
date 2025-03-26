1b407a85908efeb26cf115f663a715fe7d45335b
SziNo <szino1848@gmail.com>
2025-03-26 19:03:58 +0100

    implement modal for delete button

--- components/ClientDeleteButton.tsx
+++ components/ClientDeleteButton.tsx
@@ -1,8 +1,20 @@
 'use client'
 
 import { deleteInterviewById } from '@/lib/actions/general.action'
-import { Button } from './ui/button'
 import { useRouter } from 'next/navigation'
+import { Button } from './ui/button'
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/components/ui/alert-dialog'
+import { toast } from 'sonner'
 
 const ClientDeleteButton = ({ id }: { id: string }) => {
   const router = useRouter()
@@ -10,16 +22,37 @@ const ClientDeleteButton = ({ id }: { id: string }) => {
   const handleDelete = async () => {
     await deleteInterviewById(id)
     router.refresh()
+    toast.success('Interview deleted successfully')
   }
 
   return (
-    <Button
-      variant='destructive'
-      className='absolute top-0 left-0 cursor-pointer'
-      onClick={handleDelete}
-    >
-      X
-    </Button>
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button
+          variant='destructive'
+          className='absolute top-0 left-0 cursor-pointer hover:!bg-red-400'
+        >
+          X
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This will permanently delete your Interview and corresponding
+            feedbacks.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogAction className='cursor-pointer' onClick={handleDelete}>
+            Delete
+          </AlertDialogAction>
+          <AlertDialogCancel className='cursor-pointer'>
+            Cancel
+          </AlertDialogCancel>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   )
 }
 
